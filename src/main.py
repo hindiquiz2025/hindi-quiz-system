254e8e052641af9a2feed7a238e4b7b8a900d802
hindiquiz2025 <hindiquiz2025@gmail.com>
2025-09-01 16:41:38 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -54,6 +54,5 @@ def serve(path):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
     debug = os.environ.get('FLASK_ENV') != 'production'
-    app.run(host='0.0.0.0', port=port, debug=debug)
+    app.run(host='0.0.0.0', port=8080, debug=debug)
